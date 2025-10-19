38db25487fb6f8f33286816565c0952b298b91b9
rashsed60cyber <rashsed60@gmail.com>
2025-10-19 23:25:39 +0600

    Create text.java

---
+++ text.java
@@ -0,0 +1,9 @@
+class hello {
+    public static void main(String[] abc){
+
+        System.out.println("     J     A      V      V    A     ");
+        System.out.println("     J    A A      V    V    A A     ");
+        System.out.println(" J   J   A A A      V  V    A A A    ");
+        System.out.println(" J J    A     A      V     A     A   ");
+    }
+}
