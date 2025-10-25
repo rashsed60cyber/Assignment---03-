3df54158250155518a7ce2330acbdf780e33134a
rashsed60cyber <rashsed60@gmail.com>
2025-10-26 02:39:17 +0600

    Create area_perimeter_rectangle.java

---
+++ area_perimeter_rectangle.java
@@ -0,0 +1,14 @@
+class AreaPerimeter {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
